2f31dc733849c62fe5605e34641ee7ac40604bae
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-09-22 23:18:04 +0100

    Update to add no tile compression

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 wget "https://openbenches.org/api/benches?truncated=false&media=true" -O benches.json
 rm -rf tiles
 mkdir tiles
-tippecanoe --output-to-directory tiles benches.json
+tippecanoe --no-tile-compression --output-to-directory tiles benches.json
