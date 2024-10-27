f9c97963d95ed8357c4256b58e9e3c8ef5fb583d
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-10-27 23:50:23 +0000

    Fixing broken api link

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-wget "https://openbenches.org/api/benches?truncated=false&media=true" -O benches.json
+wget "https://openbenches.org/api/benches?truncated=false" -O benches.json
 rm -rf tiles
 mkdir tiles
 tippecanoe --no-tile-compression --output-to-directory tiles benches.json
