4c13198059c5fac790bd4dd7453e8b5377d38d9e
Dave <8709533+DaveBathnes@users.noreply.github.com>
2026-05-18 08:24:54 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-wget "https://openbenches.org/api/benches?truncated=false" -O benches.json
+wget "https://openbenches.org/api/benches?truncated=false" -O benches.json --header="Content-Type: application/json"
 rm -rf tiles
 mkdir tiles
 tippecanoe --no-tile-compression --output-to-directory tiles benches.json
